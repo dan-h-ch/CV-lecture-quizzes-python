7b947a0a31932a2660b57180c9dd98bd993fab85
dan-h-ch <chang.daniel@gmail.com>
2021-01-26 20:47:49 -0800

    Regular commit

--- 3A-L2/project_a_point.py
+++ 3A-L2/project_a_point.py
@@ -9,10 +9,23 @@ import numpy as np
 
 def project_point(p, f):
     # TODO: Define and apply projection matrix
-    pass
+    identity = np.identity(3).astype('float64')
+    projection_matrix = np.zeros((3, 4))
+    projection_matrix[:,:-1] = identity
+    projection_matrix[2,2] = 1/f
+
+    homogenous_coordinates = np.array(p)
+    homogenous_coordinates = np.append(homogenous_coordinates, [1])
+    homogenous_coordinates = homogenous_coordinates.reshape(4, 1)
+
+    product = np.matmul(projection_matrix, homogenous_coordinates)
+
+    answer = np.array([product[0,0]/product[2,0], product[1,0]/product[2,0]])
+
+    return answer
 
 # Test: Given point and focal length (units: mm)
-p = np.array([[200, 100, 120]])
+p = np.array([[200, -900, 50]])
 f = 50
 
 print (project_point(p, f))
